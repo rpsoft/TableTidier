a9d9a739081c06c7a035daf063f98556e253b298
felixdrp <felixdrp@gmail.com>
2022-04-05 12:45:23 +0100

    refactor: route '/formattedResults'

--- Server/src/index.js
+++ Server/src/index.js
@@ -1125,61 +1125,64 @@ app.post(CONFIG.api_base_url+'/annotationPreview',async function(req,res){
 
 // Returns all annotations for all document/tables.
 app.get(CONFIG.api_base_url+'/formattedResults', async function (req,res){
+  const results = await dbDriver.annotationResultsGet()
 
-       var results = await dbDriver.annotationResultsGet()
+  if ( !results || !results.rows ) {
+    return res.send('No Rows')
+  }
+  const finalResults = {}
 
-       if ( results ){
+  /**
+  * There are multiple versions of the annotations. 
+  * When calling reading the results from the database,
+  * here we will return only the latest/ most complete version of the annotation.
+  * Independently from the author of it. 
+  * Completeness here measured as the result with 
+  * the highest number of annotations and the highest index number
+  * (I.e. Newest, but only if it has more information/annotations).
+  * May not be the best in some cases.
+  */
 
-          var finalResults = {}
-
-          /**
-          * There are multiple versions of the annotations. When calling reading the results from the database, here we will return only the latest/ most complete version of the annotation.
-          * Independently from the author of it. Completeness here measured as the result with the highest number of annotations and the highest index number (I.e. Newest, but only if it has more information/annotations).
-          * May not be the best in some cases.
-          */
-
-          for ( var r in results.rows){
-            var ann = results.rows[r]
-            var existing = finalResults[ann.docid+"_"+ann.page]
-            if ( existing ){
-              if ( ann.N > existing.N && ann.annotation.annotations.length >= existing.annotation.annotations.length ){
-                    finalResults[ann.docid+"_"+ann.page] = ann
-              }
-            } else { // Didn't exist so add it.
-              finalResults[ann.docid+"_"+ann.page] = ann
-            }
-          }
-
-          var finalResults_array = []
-          for (  var r in finalResults ){
-            var ann = finalResults[r]
-            finalResults_array[finalResults_array.length] = ann
-          }
-
-          var formattedRes = '"user","docid","page","corrupted_text","tableType","location","number","content","qualifiers"\n';
-
-          finalResults_array.map( (value, i) => {
-            value.annotation.annotations.map( (ann , j ) => {
-              try {
-                formattedRes = formattedRes+ '"'+value.user
-                                          +'","'+value.docid
-                                          +'","'+value.page
-                                          // +'","'+value.corrupted
-                                          +'","'+ (value.corrupted_text == "undefined" ? "" : value.corrupted_text  ).replace(/\"/g,"'")
-                                          +'","'+value.tableType
-                                          +'","'+ann.location
-                                          +'","'+ann.number
-                                          +'","'+(Object.keys(ann.content).join(';'))
-                                          +'","'+(Object.keys(ann.qualifiers).join(';'))+'"'+"\n"
-              } catch (e){
-                console.log("an empty annotation, no worries: "+JSON.stringify(ann))
-              }
-
-            })
-          })
-
-          res.send(formattedRes)
+  for ( const r in results.rows){
+    const ann = results.rows[r]
+    const existing = finalResults[ann.docid+"_"+ann.page]
+    if ( existing ){
+      if ( ann.N > existing.N && ann.annotation.annotations.length >= existing.annotation.annotations.length ) {
+        finalResults[ann.docid+"_"+ann.page] = ann
       }
+    } else { // Didn't exist so add it.
+      finalResults[ann.docid+"_"+ann.page] = ann
+    }
+  }
+
+  let finalResults_array = []
+  for (  const r in finalResults ){
+    const ann = finalResults[r]
+    finalResults_array[finalResults_array.length] = ann
+  }
+
+  let formattedRes = '"user","docid","page","corrupted_text","tableType","location","number","content","qualifiers"\n';
+
+  finalResults_array.forEach( (value, i) => {
+    value.annotation.annotations.forEach( (ann , j ) => {
+      try {
+        formattedRes = formattedRes+ '"'+value.user
+                                  +'","'+value.docid
+                                  +'","'+value.page
+                                  // +'","'+value.corrupted
+                                  +'","'+ (value.corrupted_text == "undefined" ? "" : value.corrupted_text  ).replace(/\"/g,"'")
+                                  +'","'+value.tableType
+                                  +'","'+ann.location
+                                  +'","'+ann.number
+                                  +'","'+(Object.keys(ann.content).join(';'))
+                                  +'","'+(Object.keys(ann.qualifiers).join(';'))+'"'+"\n"
+      } catch (err) {
+        console.log("an empty annotation, no worries: "+JSON.stringify(ann))
+      }
+    })
+  })
+
+  res.send(formattedRes)
 })
 
 // app.get('/api/abs_index',function(req,res){
