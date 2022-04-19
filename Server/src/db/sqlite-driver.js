028d679ef472fe53656c17e8af9fb8c2463d1c4e
felixdrp <felixdrp@gmail.com>
2022-04-19 13:46:43 +0100

    feat: module sqlite-driver.js
    - postgres driver used as template
    - It use of sqlite3 package
    - refactored  users, table and collection methods

---
+++ Server/src/db/sqlite-driver.js
@@ -0,0 +1,564 @@
+// Environmet NODE_ENV DEV 'dev', 'prod' 
+const sqlite3 = (
+    process.env.NODE_ENV == 'test' ||
+    process.env.NODE_ENV == 'dev'
+  ) ?
+    require('sqlite3').verbose()
+    : require('sqlite3');
+console.log(process.env.NODE_ENV)
+const path = require('path');
+
+const {
+  moveFileToCollection,
+} = require('../utils/files')
+
+function driver(config) {
+  // :-) Check config has valid fields
+  console.log("Configuring DB client: Sqlite")
+  const {
+    // Database filename
+    filename,
+  } = config
+
+  // Load database handler
+  let db
+  // let db = new sqlite3.Database(':memory:');
+  const dbLoadingPromise = new Promise((resolve, reject) => {
+    // Return _db when database is open
+    const _db = new sqlite3.Database(filename, (err) => {
+      if (err) return reject(err)
+      // When loaded store local _db in global db
+      db = _db
+      resolve()
+    });
+  })
+
+  // Generic Calls
+  const query = (action, queryText, values) => {
+    return new Promise((resolve, reject) => {
+      if (!db) {
+        return reject(new Error('Database.close: database is not open'));
+      }
+      db[action](queryText, values, (err, result) => {
+        if (err) {
+          // if (err.includes('no such table'))
+          return reject(new Error('Database: '+err));
+        }
+        resolve(result)
+      })
+    })
+  }
+
+  const queryGet = (queryText, values) => query('get', queryText, values)
+  const queryAll = (queryText, values) => query('all', queryText, values)
+  const queryRun = (queryText, values) => query('run', queryText, values)
+
+
+
+  return {
+    // Return db handler
+    db: dbLoadingPromise,
+    close: () => db.close(),
+
+    // Returns the annotation for a single document/table
+    annotationByIDGet: (docid, page, collId) => {
+      if ( docid == "undefined" || page == "undefined" || collId == "undefined" ) {
+        return {rows:[]}
+      }
+
+      return query(
+      `SELECT 
+        docid,
+        "user",
+        notes,
+        collection_id,
+        file_path,
+        "tableType",
+        "table".tid,
+        completion,
+        annotation
+      FROM 
+        "table"
+      LEFT JOIN
+        annotations
+      ON
+        "table".tid = annotations.tid
+      WHERE
+      docid=$1 AND page=$2 AND collection_id = $3`,
+        [docid, page, collId]
+      ) 
+    },
+
+    annotationDataGet: (tids) => query(
+    // * :-)  Sqlite version transform "annotations".annotation from text to json
+    `SELECT 
+  docid,
+  page,
+  collection_id,
+  file_path,
+  "table".tid,
+  "annotations".annotation
+FROM 
+  "table",
+  "annotations"
+WHERE
+  "table".tid = "annotations".tid AND "table".tid = ANY ($1)`,
+      [tids]
+    ),
+
+    annotationInsert: (tid, annotation) => query(
+      'INSERT INTO annotations VALUES($2,$1) ON CONFLICT (tid) DO UPDATE SET annotation = $2;',
+      [tid, annotation]
+    ),
+
+    annotationResultsGet: async () => {
+      const result = await query(
+        `SELECT * FROM "table", annotations where "table".tid = annotations.tid order by docid desc,page asc`
+      )
+      // var filtered_rows = []
+      //
+      // var hey = available_documents
+      //
+      // var docids = Object.keys(available_documents)
+      //
+      // for ( var i=0; i < result.rows.length; i++ ) {
+      //
+      //   if (  docids.indexOf (result.rows[i].docid) > -1 ){
+      //     filtered_rows.push(result.rows[i])
+      //   }
+      //
+      // }
+      //
+      // result.rows = filtered_rows
+
+        // debugger
+      return result
+    },
+
+    collectionCreate: async (title, description, owner) => {
+      const result = await queryGet(
+        `INSERT INTO collection(
+          collection_id, title, description, owner_username, visibility, completion)
+        VALUES ((SELECT IFNULL(max(collection_id), 0) + 1 FROM collection), $1, $2, $3, $4, $5)
+        returning *;`, 
+        [title, description, owner, 'public', 'in progress']
+      )
+      // const result = await query(
+      //   `Select * from collection
+      //   ORDER BY collection_id DESC LIMIT 1;`
+      // )
+      return result
+    },
+
+    collectionDelete: async function (collection_id) {
+      try {
+        const tables = await queryAll(
+          `SELECT docid, page FROM "table" WHERE collection_id = $1`,[collection_id]
+        )
+        await this.tablesRemove(tables, collection_id, true);
+        await queryRun(
+          `DELETE FROM collection WHERE collection_id = $1`, [collection_id]
+        )
+      } catch (err) {
+        return err
+      }
+      return 'done'
+    },
+
+    collectionEdit: async (collData) => {
+      if (collData?.collection_id == undefined) return 'warning, collection_id not found'
+      if (Object.keys(collData).length == 1) return 'warning, more parameters needed' 
+      // Generate query in function of parameters.
+      const collDataFormated = {}
+      let _query = `UPDATE collection
+      SET `
+      const footer = ` WHERE collection_id=$collection_id returning *;`
+      for (const item in collData) {
+        collDataFormated['$'+item] = collData[item]
+        if (item == 'collection_id') continue
+        _query += `${item}=$${item}, `
+      }
+      _query = _query.slice(0, -2).concat(footer)
+
+      try {
+        const result = await queryGet(
+          _query,
+          collDataFormated
+        )
+        return result
+      } catch (err) {
+        return err
+      }
+    },
+
+    collectionGet: async (collection_id) => {
+      const result = await queryAll(
+        `SELECT *
+        FROM collection WHERE collection_id = $1`, [collection_id])
+  
+      const tables = await queryAll(
+        `SELECT docid, page, "user", notes, tid, collection_id, file_path, "tableType"
+        FROM "table" WHERE collection_id = $1 ORDER BY docid,page`, [collection_id])
+  
+      const collectionsList = await queryAll(
+        `SELECT * FROM collection ORDER BY collection_id`);
+
+      if ( result.length == 1) {
+        return {
+          ...result[0],
+          tables,
+          collectionsList,
+        }
+      }
+      return {}
+    },
+
+    collectionsList: async () => {
+      const result = await queryAll(
+        `SELECT collection.collection_id, title, description, owner_username, table_n
+        FROM collection
+        LEFT JOIN
+        ( SELECT collection_id, count(docid) as table_n FROM
+        ( select distinct docid, page, collection_id from "table" ) as interm
+        group by collection_id ) as coll_counts
+        ON collection.collection_id = coll_counts.collection_id ORDER BY collection.collection_id`
+      )
+      return result
+    },
+
+    cuiInsert: async (cui, preferred, hasMSH) => query(
+      `INSERT INTO cuis_index(cui,preferred,"hasMSH",user_defined,admin_approved)
+      VALUES ($1, $2, $3, $4, $5) ON CONFLICT (cui) DO UPDATE 
+      SET preferred = $2, "hasMSH" = $3, user_defined = $4, admin_approved = $5`,
+      [cui,preferred,hasMSH,true,false]
+    ),
+
+    cuiDataModify: async (cui, preferred, adminApproved, prevcui) => {
+      let result = await query(`UPDATE cuis_index SET cui=$1, preferred=$2, admin_approved=$3 WHERE cui = $4`,
+        [cui, preferred, adminApproved, prevcui] )
+
+      if ( result && result.rowCount ){
+        const q = new Query(
+          `UPDATE metadata 
+          SET
+            cuis = array_to_string(array_replace(regexp_split_to_array(cuis, ';'), $2, $1), ';'),
+            cuis_selected = array_to_string(array_replace(regexp_split_to_array(cuis_selected, ';'), $2, $1), ';')`,
+          [cui, prevcui]
+        )
+        result = await query( q )
+      }
+
+      return result
+    },
+
+    cuiDeleteIndex: (cui) => query('delete from cuis_index where cui = $1', [cui]),
+
+    cuiRecommend: async () => {
+      const result = await query(`select * from cuis_recommend`)
+      return result.rows
+    },
+
+    cuisIndexGet: async () => {
+      const cuis = {}
+      const result = await query(`select * from cuis_index ORDER BY preferred ASC`)
+      result.rows.forEach( row => {
+        cuis[row.cui] = {preferred : row.preferred, hasMSH: row.hasMSH, userDefined: row.user_defined, adminApproved: row.admin_approved}
+      })
+    
+      return cuis
+    },
+
+    cuiMetadataGet: (cui) => query(`select docid,page,"user" from metadata where cuis like $1 `, ["%"+cui+"%"]),
+
+    metadataClear: (tid) => query('DELETE FROM metadata WHERE tid = $1', [tid]),
+
+    metadataGet: (tids) => query(`SELECT * FROM metadata WHERE tid = ANY ($1)`,[tids]),
+
+    metadataSet: (
+      concept_source,
+      concept_root,
+      concept,
+      cuis,
+      cuis_selected,
+      qualifiers,
+      qualifiers_selected,
+      istitle,
+      labeller,
+      tid
+    ) => query(`
+    INSERT INTO metadata(concept_source, concept_root, concept, cuis, cuis_selected, qualifiers, qualifiers_selected, istitle, labeller, tid)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+    ON CONFLICT (concept_source, concept_root, concept, tid)
+    DO UPDATE SET cuis = $4, cuis_selected = $5, qualifiers = $6, qualifiers_selected = $7, istitle = $8, labeller = $9`,
+    [
+      concept_source,
+      concept_root,
+      concept,
+      cuis,
+      cuis_selected,
+      qualifiers,
+      qualifiers_selected,
+      istitle,
+      labeller,
+      tid
+    ]),
+
+    // Gets the labellers associated w ith each document/table.
+    metadataLabellersGet: () => query(`SELECT distinct docid, page, labeller FROM metadata`),
+
+    notesUpdate: (docid, page, collid, notes, tableType, completion) => query(
+      `UPDATE public."table"
+      SET
+        notes=$4,
+        "tableType"=$5,
+        completion=$6
+      WHERE
+        docid=$1 AND page=$2 AND collection_id=$3`,
+      [docid, page, collid, notes, tableType, completion]
+    ),
+
+    // resource = {type: [collection or table], id: [collection or table id]}
+    permissionsResourceGet: async (resource, user) => {
+      let permissions;
+
+      switch (resource) {
+        case "collections":
+          permissions = await query(
+            `select *,
+            (owner_username = $1) as write,
+            (visibility = 'public' OR owner_username = $1) as read
+            from collection`,
+            [user]
+          )
+          break;
+        case "table":
+          break;
+        default:
+    
+      }
+    
+      return permissions?.rows.reduce( (acc, row) => {
+        if (row.read) {
+          acc.read.push(row.collection_id)
+        }
+        if (row.write) {
+          acc.write.push(row.collection_id)
+        }
+        return acc
+      },
+        {read:[], write:[]}
+      )
+    },
+
+    resultsDataGet: (tids) => query(`SELECT * FROM "result" WHERE tid = ANY ($1)`, [tids]),
+
+    tableCreate: async (docid, page, user, collection_id, file_path) => {
+      // not valid parameters?
+      if (!!(docid && page && user && collection_id && file_path) == false) {
+        return `parameters no valid`
+      }
+      try {
+        await queryRun(
+          `INSERT INTO "table"(
+             tid, docid, page, "user", notes, collection_id, file_path, "tableType")
+           VALUES ((SELECT IFNULL(max(tid), 0) + 1 FROM "table"), $1, $2, $3, $4, $5, $6, $7);`,
+          [docid, page, user, '', collection_id, file_path, '']
+          )
+      } catch (err) {
+        return err
+      }
+      return 'done'
+    },
+
+    tablesMove: async (tables, collection_id, target_collection_id) => {
+      if (Array.isArray(tables) == false) return 'tables not valid, array expected'
+      if (tables.length == 0) return 'tables empty'
+
+      let tablesDocidPage
+      try {
+        tablesDocidPage = tables.map( (tab, index) => {
+          const [docid, page] = tab.split("_");
+          // if invalid docid or page throw err
+          if (!docid || !page) {
+            throw `Param docid=${docid} or page=${page} not valid at index ${index}`
+          }
+          return {docid, page}
+        })
+      } catch(err) {
+        return err
+      }
+
+      for ( let table of tablesDocidPage) {
+        const {
+          docid,
+          page,
+        } = table
+
+        await queryRun(
+          `UPDATE "table"
+          SET collection_id=$4
+          WHERE docid = $1 AND page = $2 AND collection_id = $3;`,
+          {
+            $1: docid,
+            $2: page,
+            $3: collection_id,
+            $4: target_collection_id
+          })
+  
+        const filename = `${docid}_${page}.html`;
+        try{
+          await moveFileToCollection(
+            {
+              originalname: filename,
+              path: path.join(
+                collection_id.toString(),
+                filename
+              )
+            },
+            target_collection_id
+          )
+        } catch (err){
+          const errorText = `MOVE FAil: ` + err + JSON.stringify(err)
+          return errorText
+        }
+      }
+  
+      return 'done'
+    },
+
+    tablesRemove: async (tables, collection_id, fromSelect = false) => {
+      if (Array.isArray(tables) == false) return 'tables not valid, array expected'
+      if (tables.length == 0) return 'tables empty'
+
+      let tablesDocidPage
+      try {
+        if (!fromSelect) {
+          tablesDocidPage = tables.map( (tab, index) => {
+            const [docid, page] = tab.split("_");
+            // if invalid docid or page throw err
+            if (!docid || !page) {
+              throw `Param docid=${docid} or page=${page} not valid at index ${index}`
+            }
+            return {docid, page}
+          })
+        }
+      } catch(err) {
+        return err
+      }
+  
+      for (let table of tablesDocidPage) {
+        const {
+          docid,
+          page,
+        } = table
+        try {
+          await queryRun(
+            `DELETE FROM "table"
+            WHERE docid = $1 AND page = $2 AND collection_id = $3;`,
+            [docid, page, collection_id]
+          )
+        } catch (err) {
+          return err
+        }
+  
+        const filename = `${docid}_${page}.html`;
+
+        try{
+          await moveFileToCollection(
+            {
+              originalname: filename,
+              path: path.join(
+                collection_id.toString(),
+                filename
+              )
+            },
+            'deleted' 
+          )
+        } catch (err) {
+          const errorText = `REMOVE FAil: ` + err + JSON.stringify(err)
+          return errorText
+        }
+      }
+      return 'done'
+    },
+
+    // Table id Get
+    // important.
+    // Use this to recover the table id (tid).
+    // tid is used as primary key in many tables.
+    // uniquely identifying tables across sql tables.
+    tidGet: async (docid, page, collId) => {
+      if (
+        docid == "undefined" ||
+        page == "undefined" || 
+        collId == "undefined" 
+      ) {
+        return -1
+      }
+      let tid = await queryGet(
+        `SELECT tid FROM "table" WHERE docid = $1 AND page = $2 AND collection_id = $3`,
+        [docid, page, collId]
+      )
+      if (!tid) {
+        return 'not found'
+      }
+      return tid.tid.toString()
+    },
+
+    userCreate: async (userData) => {
+      const {
+        username,
+        password,
+        displayName,
+        email,
+      } = userData
+      
+      if (
+        username == "undefined" ||
+        password == "undefined" ||
+        email == "undefined" 
+      ) {
+        return {code: 'invalid_parameters', error: 'invalid user data'}
+      }
+
+      let result = await queryRun(
+        `INSERT INTO users
+        (id, username, password, "displayName", email, registered, role)
+        VALUES ((SELECT IFNULL(max(id), 0) + 1 FROM users), $1, $2, $3, $4, $5, $6)`,
+        [
+          username,
+          password,
+          displayName,
+          email,
+          Date.now(),
+          "standard"
+        ]
+      );
+
+      return 'done'
+    },
+    
+    userDelete: async (email) => {
+      const user = await queryRun(`
+      DELETE FROM users
+	    WHERE email=$1`, [email])
+      return 'done'
+    },
+
+    userGet: async (email) => {
+      const user = await queryGet(`
+      SELECT
+        id, username, password, "displayName", email, registered, role
+      FROM users
+      WHERE email = $1`, [email])
+      return user
+    },
+
+    usersGet: async () => {
+      const users = await queryAll('SELECT id, username, password, "displayName", email, registered, role FROM users')
+      return users
+    },
+  }
+}
+
+module.exports = driver
