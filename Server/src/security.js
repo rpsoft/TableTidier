794f23e8effc0b2944300b2ea7840629fc65f823
felixdrp <felixdrp@gmail.com>
2022-05-27 10:37:42 +0100

    refactor: security
    - Added check user logic to 'signup'
    - Clean some code

--- Server/src/security.js
+++ Server/src/security.js
@@ -3,29 +3,43 @@ const passport = require('passport');
 import passportCustom from 'passport-custom';
 const CustomStrategy = passportCustom.Strategy;
 
-// // https://github.com/jaredhanson/passport-local
+// https://github.com/jaredhanson/passport-local
 // const localStrategy = require('passport-local').Strategy;
 
 let dbDriver = {}
 // Pass dbDriver to access DB
 export function passportAddDriver(driver) { dbDriver = driver }
+
+// Create user
 passport.use(
   'signup',
   new CustomStrategy(
     async (req, done) => {
       const {
+        email,
         username,
         password,
       } = req.body
 
-      const FAIL_GENERIC_MESSAGE = 'user or password not valid'
+      const FAIL_GENERIC_MESSAGE = 'email, username or password are not valid'
 
-      if (!username || !password) {
+      if (
+        !email ||
+        !username ||
+        !password
+      ) {
         return done(null, false, { message: FAIL_GENERIC_MESSAGE });
       }
 
       try {
-        const user = await dbDriver.userCreate({ email, password });
+        // check if user exists
+        const emailIsUsed = await dbDriver.userGet({ email });
+        const usernameIsUsed = await dbDriver.userGet({ username });
+        if (emailIsUsed || usernameIsUsed) {
+          return done(null, false, 'email or username not available');
+        }
+
+        const user = await dbDriver.userCreate({ email, username, password });
 
         return done(null, user);
       } catch (error) {
@@ -51,10 +65,6 @@ passport.use(
     }
 
     try {
-      console.log('login')
-      if (password == '$argon2id$v=19$m=256,t=128,p=1$Q2VyYmVyb3M$k9fqEi5T2bWEfPEws8aH71R2nTk4nOIG/uWghCV6OVQ') {
-        console.log('hola Felix!!')
-      }
       // Find user
       const user = await dbDriver.userGet({username});
 
@@ -83,65 +93,11 @@ passport.use(
           `role:${user.role}`
         ],
       }
-      // debugger
       return done(null, {user, jwt: userJWT}, { message: 'Logged in Successfully' });
     } catch (error) {
       return done(error);
     }
   })
 );
-
-// var passport = require('passport');
-// const crypto = require('crypto');
-//
-// import passportCustom from 'passport-custom';
-// const CustomStrategy = passportCustom.Strategy;
-//
-// global.records = [
-//   //   { id: 1, username: 'jack', password: 'secret', displayName: 'Jack', email: 'jack@example.com', registered : "1588283579685", role: "viewer" }
-//   // , { id: 2, username: 'jill', password: 'birthday', displayName: 'Jill', email:'jill@example.com', registered : "1588283575644", role: "user" }
-//   // , { id: 3, username: 'suso', password: 'me', displayName: 'Jesus', email: 'suso@example.com', registered : "1588283589667", role: "admin" }
-// ];
-//
-// export function getUserHash(user){
-//   const hash = crypto.createHmac('sha256', CONFIG.hashSecret)
-//                      .update(user.username + user.password + user.registered)
-//                      .digest('hex');
-//
-//   return {
-//     username: user.username,
-//     password: user.password,
-//     hash
-//   }
-// }
-//
-// passport.use(new CustomStrategy(
-//   function(req, done) {
-//
-//     for ( var i in global.records ){
-//       if ( global.records[i].username == req.body.username ){
-//         if (global.records[i].password != req.body.password) {
-//           return done(null, false);
-//         }
-//         return done(null, getUserHash(records[i]));
-//       }
-//     }
-//
-//     return done(null, false);
-//   }
-// ));
-//
-// passport.serializeUser(function(user, cb) {
-//   cb(null, user.id);
-// });
-//
-// passport.deserializeUser(function(id, cb) {
-//   for ( var i in global.records ){
-//     if ( global.records[i].id == id ){
-//       return cb(null, global.records[i]);
-//     }
-//   }
-//   return cb(null,false)
-// });
 
 export default passport;
