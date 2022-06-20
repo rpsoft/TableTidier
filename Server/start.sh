2892423929f7dc9f27c68ec1a160d716c19ec608
Jesus <jrp7u@ihwserver.mvls.gla.ac.uk>
2022-06-20 18:40:52 +0100

    chore: Added server start script

---
+++ Server/start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SESSION=$USER
+
+#Import configurations from config.json.
+export API_PORT=$(cat config.json | jq '.api_port')
+export API_BASE=$(cat config.json | jq '.api_base_url')
+export API_DOMAIN=$(cat config.json | jq '.api_host')
+
+export UI_PORT=$(cat config.json | jq '.ui_port')
+export UI_DOMAIN=$(cat config.json | jq '.ui_host')
+
+node index.js
