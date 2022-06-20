3b2dd6db0f65029799f070ca51390e090f519c5e
felixdrp <felixdrp@gmail.com>
2022-06-20 17:38:48 +0100

    chore: updated db dump script

--- backup_db.sh
+++ backup_db.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-pg_dump --host localhost --port 5432 --username "postgres" --role "postgres" --no-password  --format plain --encoding UTF8 --verbose --file $1 "ihw_annotator"
+# pg_dump --host localhost --port 5432 --username "postgres" --role "postgres" --no-password  --format plain --encoding UTF8 --verbose --file $1 "ihw_annotator"
+pg_dump --host localhost --port 5432 --username "postgres" --role "postgres" --format plain --encoding UTF8 --verbose --file $1 "ihw_annotator"
