c5321743470debbffd5b25febdf9104d862f8896
felixdrp <felixdrp@gmail.com>
2022-07-01 13:01:33 +0100

    refactor: some code styling

--- UI/app/components/TableMetadata/index.js
+++ UI/app/components/TableMetadata/index.js
@@ -31,8 +31,6 @@ import {
 
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 
-const _ = require('lodash');
-
 import AdbIcon from '@material-ui/icons/Adb';
 
 import TableMetadataItem from 'components/TableMetadataItem'
@@ -49,20 +47,17 @@ function TableMetadata({
   allowEdit
 }) {
 
-
-  var hey = _;
-
   const [ enableDelete, setEnableDelete ] = React.useState(false)
 
   const [ enableMetadataAdder, setMetadataAdder ] = React.useState(false)
-  const [ metadataAdderText, setMetadataAdderText ] = React.useState("")
+  const [ metadataAdderText, setMetadataAdderText ] = React.useState('')
 
   const toggleCui = (key, cui) => {
 
-    var cuipos = metadata[key].cuis_selected.indexOf(cui)
+    const cuipos = metadata[key].cuis_selected.indexOf(cui)
 
-    if ( cuipos > -1){
-      metadata[key].cuis_selected.splice(cuipos,1)
+    if ( cuipos > -1 ) {
+      metadata[key].cuis_selected.splice(cuipos, 1)
     } else {
       metadata[key].cuis_selected.push(cui)
     }
@@ -70,35 +65,30 @@ function TableMetadata({
     updateTableMetadata(Object.assign({}, metadata))
   }
 
-
   const addCuis = (key, cuis, conceptData) => {
-
     cuis.map( cui => {
-
       if (!metadata[key]){
-
-        var tres = tableResults
          // debugger
         // metadata concept has not been annotated yet.
         metadata[key] = {
           concept: conceptData.concept,
           concept_root: conceptData.root,
-          concept_source: "", // This legacy.
+          concept_source: '', // This legacy.
           cuis: [],
           cuis_selected: [],
           istitle: false,
-          labeller: "",
+          labeller: '',
           qualifiers: [],
           qualifiers_selected: [],
           tid: tid,
         }
       }
 
-      var cuipos = metadata[key].cuis.indexOf(cui)
+      const cuipos = metadata[key].cuis.indexOf(cui)
 
       if ( cuipos < 0 ){
-          metadata[key].cuis.push(cui)
-          metadata[key].cuis_selected.push(cui)
+        metadata[key].cuis.push(cui)
+        metadata[key].cuis_selected.push(cui)
       }
     })
 
@@ -106,7 +96,7 @@ function TableMetadata({
   }
 
   const deleteCui = (key, cui) => {
-    var cuipos = metadata[key].cuis_selected.indexOf(cui)
+    let cuipos = metadata[key].cuis_selected.indexOf(cui)
 
     if ( cuipos > -1){
       metadata[key].cuis_selected.splice(cuipos,1)
@@ -121,74 +111,88 @@ function TableMetadata({
     updateTableMetadata(Object.assign({}, metadata))
   }
 
-  var manualMetadata = () => {
-    return Object.keys(metadata).reduce( (acc,k) => { if ( metadata[k].istitle ) {acc.push(metadata[k]);} return acc }, [])
+  const manualMetadata = () => {
+    return Object.keys(metadata).reduce(
+      (acc,k) => { if ( metadata[k].istitle ) {acc.push(metadata[k]);} return acc },
+      []
+    )
   }
 
   const addTitleMetadataConcept = () => {
-    var newMetadata = Object.assign({}, metadata)
+    const newMetadata = Object.assign({}, metadata)
 
-    if ( metadataAdderText.toLowerCase().trim().length > 0){
+    if ( metadataAdderText.toLowerCase().trim().length > 0) {
       newMetadata[metadataAdderText.toLowerCase().trim()] = {
-          concept: metadataAdderText.trim(),
-          concept_root: "",
-          concept_source: "",
-          cuis: [],
-          cuis_selected: [],
-          istitle: true,
-          labeller: "suso",
-          qualifiers: [],
-          qualifiers_selected: [],
-          tid: tid,
-        }
+        concept: metadataAdderText.trim(),
+        concept_root: '',
+        concept_source: '',
+        cuis: [],
+        cuis_selected: [],
+        istitle: true,
+        labeller: 'suso',
+        qualifiers: [],
+        qualifiers_selected: [],
+        tid: tid,
+      }
     }
 
     updateTableMetadata(newMetadata);
     setMetadataAdder(false);
-    setMetadataAdderText("");
+    setMetadataAdderText('');
   }
 
-
   const removeTitleMetadataConcept = (concept) => {
-      var newMetadata = Object.assign({}, metadata)
+    const newMetadata = structuredClone(metadata)
 
-      delete newMetadata[concept.toLowerCase().trim()];
+    delete newMetadata[concept.toLowerCase().trim()];
 
-      updateTableMetadata(newMetadata);
-      // setMetadataAdder(false);
-    }
+    updateTableMetadata(newMetadata);
+    // setMetadataAdder(false);
+  }
 
   const onKeyDown = (event) => {
-      if (event.key === 'Enter') {
-        event.preventDefault();
-        event.stopPropagation();
-        addTitleMetadataConcept();
-      }
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      event.stopPropagation();
+      addTitleMetadataConcept();
+    }
   }
 
   return (
     <div style={{padding:"5px 7px 7px 7px"}} >
 
       <div style={{marginBottom:10}}>
+        {
+        allowEdit && (
+        <Button
+          variant="outlined"
+          style={{backgroundColor:"lightblue", height: 40, float:"right"}}
+          onClick={ () => { saveMetadataChanges(metadata);} }
+        > Save Metadata Changes </Button> )}
 
+        {
+        allowEdit && (
+        <Button
+          variant="outlined"
+          style={{backgroundColor:"lightblue", marginRight:10, height: 40, float:"right"}}
+          onClick={ () => { autoLabel(); } }
+        > Auto Label <AdbIcon /> </Button> )}
 
-        {allowEdit ? <Button variant="outlined" style={{backgroundColor:"lightblue", height: 40, float:"right"}} onClick={ () => { saveMetadataChanges(metadata);} }> Save Metadata Changes </Button> : ""}
-
-        {allowEdit ? <Button variant="outlined" style={{backgroundColor:"lightblue", marginRight:10, height: 40, float:"right"}} onClick={ () => { autoLabel(); } }> Auto Label <AdbIcon /> </Button> : ""}
-
-        {allowEdit ? <span style={{float:"right"}}><div style={{marginRight:10, fontSize:17, border:"1px #acacac solid", borderRadius:10, paddingLeft:10}}>
-          Enable Delete
-          <Switch
+        {
+        allowEdit && (
+        <span style={{float:"right"}}>
+          <div style={{marginRight:10, fontSize:17, border:"1px #acacac solid", borderRadius:10, paddingLeft:10}}>
+            Enable Delete
+            <Switch
               checked={enableDelete}
               onChange={() => { setEnableDelete(!enableDelete) }}
               name="checkedA"
               inputProps={{ 'aria-label': 'secondary checkbox' }}
-
             />
-        </div></span> : ""}
+          </div>
+        </span> )}
 
         <div style={{height:35, fontSize:22, paddingTop:5}}> 4. <b> Terminology </b> Linking </div>
-
       </div>
       {
         ///updateTableMetadata(Object.assign({}, metadata))
@@ -197,14 +201,17 @@ function TableMetadata({
       <div style={{width:"100%"}}>
         <h3 style={{display:"inline"}}>Other metadata</h3>
 
-          {allowEdit ? <Button variant="outlined"
-                style={{height: 40, display:"inline", marginLeft:10, color:"green"}}
-                onClick={ () => { setMetadataAdder(!enableMetadataAdder);} }> + Add other terminology </Button> :""}
+        {
+          allowEdit && (
+          <Button variant="outlined"
+            style={{height: 40, display:"inline", marginLeft:10, color:"green"}}
+            onClick={ () => { setMetadataAdder(!enableMetadataAdder);} }
+          > + Add other terminology </Button>)
+        }
 
         {
-          enableMetadataAdder ? <div>
+          enableMetadataAdder && <div>
             <div  style={{border:"1px solid black", padding:10, margin:5, marginLeft:20, display:"inline-block"}} >
-
 
               <TextField id="standard-basic" label="Enter concept text..." variant="outlined" style={{width:550}}
                 value={metadataAdderText}
@@ -221,38 +228,39 @@ function TableMetadata({
               <Button variant="outlined"
                         style={{height: 40, display:"inline", marginLeft:10, marginTop:8}}
                         onClick={ () => {  setMetadataAdder(false);} }> Cancel </Button>
-              </div>
-          </div> : ""
+            </div>
+          </div>
         }
       </div>
 
       {
+      manualMetadata().map( (item,i) => <div key={i}> {
+        <div >
+          {
+            enableDelete && (
+              <Button
+                variant="outlined"
+                style={{height: 40, marginLeft:5, marginRight:5, color:"red", float:"left"}}
+                onClick={ () => {  removeTitleMetadataConcept( item.concept ); } }
+              > Delete concept <DeleteForeverIcon/>
+              </Button> )
+          }
 
-          manualMetadata().map( (item,i) => <div key={i}> {
-            <div >
-              {
-                enableDelete ? <Button variant="outlined"
-                        style={{height: 40, marginLeft:5, marginRight:5, color:"red",float:"left"}}
-                        onClick={ () => {  removeTitleMetadataConcept( item.concept ); } }> Delete concept <DeleteForeverIcon/> </Button> : ""
-              }
-
-              <TableMetadataItem
-                              key={ i }
-                              keyN={ i }
-                              tableConcept={ [item.concept_root, item.concept] }
-                              metadata={ metadata }
-                              cuisIndex={ cuisIndex }
-                              toggleCui={ toggleCui }
-                              addCuis={ addCuis }
-                              deleteCui={ deleteCui }
-                              enableDelete={ enableDelete }
-                              allowEdit= {allowEdit}
-                          />
-            </div>
-          } </div> )
+          <TableMetadataItem
+            key={ i }
+            keyN={ i }
+            tableConcept={ [item.concept_root, item.concept] }
+            metadata={ metadata }
+            cuisIndex={ cuisIndex }
+            toggleCui={ toggleCui }
+            addCuis={ addCuis }
+            deleteCui={ deleteCui }
+            enableDelete={ enableDelete }
+            allowEdit= {allowEdit}
+          />
+        </div>
+      } </div> )
       }
-
-
 
       <hr style={{borderTop:"1px #acacac dashed"}}/>
       {
@@ -263,17 +271,17 @@ function TableMetadata({
                 (item, i) => {
                    // debugger
                   return <TableMetadataItem
-                                  key={ i }
-                                  keyN={ i }
-                                  tableConcept={ item }
-                                  metadata={ metadata }
-                                  cuisIndex={ cuisIndex }
-                                  toggleCui={ toggleCui }
-                                  addCuis={ addCuis }
-                                  deleteCui={ deleteCui }
-                                  enableDelete={ enableDelete }
-                                  allowEdit= {allowEdit}
-                              />}
+                            key={ i }
+                            keyN={ i }
+                            tableConcept={ item }
+                            metadata={ metadata }
+                            cuisIndex={ cuisIndex }
+                            toggleCui={ toggleCui }
+                            addCuis={ addCuis }
+                            deleteCui={ deleteCui }
+                            enableDelete={ enableDelete }
+                            allowEdit= {allowEdit}
+                          />}
               )} </div></div>
         })
       }
