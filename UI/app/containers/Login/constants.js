c6296eecbaaf7f40a0ecc4d13bdc51fa8cb5d012
felixdrp <felixdrp@gmail.com>
2022-07-25 20:40:51 +0100

    refactor: removed LOGIN_UPDATE_TOKEN
    - Now jwt token is stored in the client cookie
    - Refresh token jwt is stored at localStorage

--- UI/app/containers/Login/constants.js
+++ UI/app/containers/Login/constants.js
@@ -10,6 +10,4 @@ export const LOGIN_ACTION_SUCCESS = 'app/Login/LOGIN_ACTION_SUCCESS';
 
 export const LOGIN_ACTION_FAILED = 'app/Login/LOGIN_ACTION_FAILED';
 
-export const LOGIN_UPDATE_TOKEN = 'app/Login/LOGIN_UPDATE_TOKEN';
-
 export const LOGOUT_ACTION = 'app/Login/LOGOUT_ACTION';
