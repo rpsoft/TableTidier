be6cfdb2d938e7398d9b4db3a8e9920e04fb5c5e
felixdrp <felixdrp@gmail.com>
2022-08-22 11:44:13 +0100

    refactor: Collection component
    - Restructured html (jsx) code
    - Moved styles to  classes
    - Added comments

--- UI/app/components/Collection/index.js
+++ UI/app/components/Collection/index.js
@@ -4,7 +4,11 @@
  *
  */
 
-import React, { memo, useState, useEffect } from 'react';
+import React, {
+  memo,
+  // useState,
+  // useEffect
+} from 'react';
 // import PropTypes from 'prop-types';
 // import styled from 'styled-components';
 import {
@@ -18,7 +22,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import CollectionIcon from '@material-ui/icons/DynamicFeed';
 
 import IconButton from '@material-ui/core/IconButton';
-
+// import PersonIcon from '@material-ui/icons/Person';
 import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 
 import {
@@ -27,9 +31,26 @@ import {
 } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
+  collectionCard: {
+    padding:10,
+    margin:5,
+  },
   link: {
-    color: 'inherit',
+    marginBottom:10,
+    fontSize:20,
+    fontWeight: 'bold',
+    color: '#4e4e4e',
     textDecoration: 'none',
+    '&:hover': {
+      color: 'black',
+    },
+    '& > div': {
+      '&:hover':{
+        backgroundColor: '#f7f7f7',
+        borderTopLeftRadius: 5,
+        borderTopRightRadius: 5,
+      }
+    },
   }
 }));
 
@@ -42,31 +63,31 @@ function Collection({
   collectionUrl,
 }) {
   const classes = useStyles();
-  const [tables, setTables] = useState({})
-
-  const loadTables = () => {
-
-  }
 
   return (
-    <Card style={{padding:10,margin:5, fontSize: 18}}><div>
-      <div style={{fontSize:30,marginBottom:10}}>
-        <CollectionIcon fontSize={"large"}/>
-        <Link to={collectionUrl}  className={classes.link}>
-          {col_id+". "} { title || "default title"}
-        </Link>
-      </div>
-      <div style={{marginTop:5, marginLeft:5}}> Description: { description || "default description"} </div>
-      <div style={{marginTop:5, marginLeft:5}}> Owner: { owner_username || "default owner"} </div>
-      <div style={{marginTop:5, marginLeft:5}}> { table_n || "0"} tables </div>
-      <Link to={collectionUrl} >
-        <Button variant="contained" disableElevation
-          style={{float:"right"}}
-        >
-          Enter Collection <ArrowForwardIcon />
-        </Button>
+    <Card className={classes.collectionCard}>
+      {/* Header */}
+      <Link to={collectionUrl} className={classes.link}>
+        <div>
+          <CollectionIcon fontSize={"small"}/>
+          {' '+col_id+'. '} { title || "default title"}
+        </div>
       </Link>
-    </div></Card>
+
+      <div style={{marginTop:5, marginLeft:27, fontSize: 17}}>
+        Description: { description || "default description"}
+      </div>
+
+      {/* Info: number of tables and user owner */}
+      <div style={{marginTop:5, marginLeft:5, fontSize: 15}}>
+        <span>
+          { table_n || '0'} tables
+        </span>
+        <span style={{marginLeft: 15, marginRight: 5}}>
+          Owner: { owner_username || 'default owner'}
+        </span>
+      </div>
+    </Card>
   );
 }
 
