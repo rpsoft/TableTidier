b101977616780c4eb07af6de158b191398a34ba6
felixdrp <felixdrp@gmail.com>
2022-10-05 12:22:34 +0100

    refactor: MultiplePopOver
    - Prop 'options' input moved from Arrays to Objects.
    This way we can set a value for processing and a text for UI display.
    - Also it analyse if checked var is present at the props options.
    If it is not present it will add it to options as an option.

--- UI/app/components/MultiplePopOver/index.js
+++ UI/app/components/MultiplePopOver/index.js
@@ -27,69 +27,96 @@ function MultiplePopOver({
   style
 }) {
 
-    const [open, setOpen] = React.useState(false);
-    const [checked, setChecked] = React.useState( value || []);
-    const [anchorEl, setAnchorEl] = React.useState();
+  const [open, setOpen] = React.useState(false);
+  const [checked, setChecked] = React.useState( value || []);
+  const [anchorEl, setAnchorEl] = React.useState();
 
-    React.useEffect(
-    () => {
-      setChecked(value)
-    }, [value]);
+  React.useEffect( () => {
+    setChecked(value)
+  }, [value]);
 
+  const handleClick = (event) => {
+    setOpen(true)
+    setAnchorEl(event.currentTarget)
+  };
 
-    const handleClick = (event) => {
-      setOpen(true)
-      setAnchorEl(event.currentTarget)
-    };
+  const handleRequestClose = () => {
+    setOpen(false)
+  };
 
-    const handleRequestClose = () => {
-      setOpen(false)
-    };
+  const updateCheck = (value) => {
+    const newChecked = checked
+    const ind = newChecked.indexOf(value)
 
-    const updateCheck = (value) => {
-      var newChecked = checked
-      var ind = newChecked.indexOf(value)
-
-      if ( ind > -1 ){
-        newChecked.splice(ind,1)
-      } else {
-        newChecked.push(value)
-      }
-
-      setChecked(Array.from(newChecked))
-      updateAnnotation(newChecked)
+    if ( ind > -1 ) {
+      newChecked.splice(ind, 1)
+    } else {
+      newChecked.push(value)
     }
 
-    const labelname = checked.length > 0 ? checked.join(", ") : (variable || "undefined")
+    setChecked(Array.from(newChecked))
+    updateAnnotation(newChecked)
+  }
 
-    return (
-      <div style={Object.assign({}, style ,{display:"inline"})}>
-        <Button onClick={handleClick} variant={"contained"}>{labelname}</Button>
-        <Popover
-          open={open}
-          anchorEl={anchorEl}
-          anchorOrigin={{horizontal: 'left', vertical: 'bottom'}}
-          onClose={handleRequestClose}
-        >
-          <div style={{margin:5, textAlign: "center"}}>
-            <div style={{color:"grey"}}>
-              <div style={{textAlign:"center",marginTop:12}}>Choose options</div>
-              <hr/>
-            </div>
-            {
-              options.map(
-                (v,o) => {return <div key={o} style={{marginLeft:15}}>{v}
-                            <Checkbox
-                              value={ v }
-                              checked={ checked.indexOf(v) > -1 }
-                              onChange={ () => { updateCheck(v) }}
-                              />
-                         </div>}
-                       )
-            }
+  const labelname = checked.length > 0 ? checked.join(', ') : (variable || 'undefined')
+
+  let parameters = Object.entries(options)
+  const parametersKeys = Object.keys(options)
+
+  // Add custom paramaters to parameters
+  // check if checked parameter is a default parameter or it is a custom
+  const parametersCheckedCustomNotDefault = checked.filter(element => 
+    parametersKeys.includes(element) == false
+  )
+ 
+  // If parameters is not in default then add those parameters
+  if (
+    Array.isArray(parametersCheckedCustomNotDefault) &&
+    parametersCheckedCustomNotDefault.length > 0
+  ) {
+    parametersCheckedCustomNotDefault.forEach(
+      // format [key, text] example: ['feature', 'characteristics (features)']
+      parameterCustom => parameters.push([parameterCustom, parameterCustom])
+    )
+  }
+
+  return (
+    <div style={Object.assign({}, style ,{display:"inline"})}>
+      <Button onClick={handleClick} variant={"contained"}>{labelname}</Button>
+      <Popover
+        open={open}
+        anchorEl={anchorEl}
+        anchorOrigin={{horizontal: 'left', vertical: 'bottom'}}
+        onClose={handleRequestClose}
+      >
+        <div style={{margin:5, textAlign: "center"}}>
+          <div style={{color:"grey"}}>
+            <div style={{textAlign:"center",marginTop:12}}>Choose options</div>
+            <hr/>
           </div>
-        </Popover>
-      </div>);
+
+          {
+            parameters.map(
+              parameter => {
+                const [key, text] = parameter
+                return (
+                  <div
+                    key={key}
+                    style={{marginLeft:15}}
+                  >{text}
+                    <Checkbox
+                      value={ key }
+                      checked={ checked.includes(key) == true }
+                      onChange={ () => { updateCheck(key) } }
+                    />
+                  </div>
+                )
+              }
+            )
+          }
+        </div>
+      </Popover>
+    </div>);
 }
 
 MultiplePopOver.propTypes = {};
