226052ea25caee5cf65a239f236884d033d7698f
felixdrp <felixdrp@gmail.com>
2022-10-27 13:10:37 +0100

    chore: updated babeljs config file

--- Server/babel.config.js
+++ Server/babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
-  presets: [['@babel/preset-env', {targets: {node: 'current'}}]],
+  // https://babeljs.io/docs/en/assumption
+  assumptions: {
+    iterableIsArray: true,
+  },
+  presets: [
+    [
+      '@babel/preset-env',
+      // {
+      //   "useBuiltIns": "entry",
+      //   "corejs": "3.26",
+      //   loose: true,
+      // },
+      // {targets: {node: 'current'}}
+    ]
+  ],
 };
