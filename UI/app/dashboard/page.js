f5006e6b2ee3c6e6ce342ff84bb1c8bd4803713f
Jesus <rpsoft@gmail.com>
2024-05-09 16:45:43 +0100

    using nextJS

---
+++ UI/app/dashboard/page.js
@@ -0,0 +1,6 @@
+import {Button} from 'antd'
+
+// `app/page.js` is the UI for the `/` URL
+export default function Page() {
+    return <><h1>Hello, Home page!</h1><Button type="primary">Primary Button</Button></>
+}
