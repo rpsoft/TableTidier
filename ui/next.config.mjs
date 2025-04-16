a29415496110d956b817e3c909646f48137e5180
Jesus Alberto Rodriguez Perez <jesus.rodriguezperez@datasky.uk>
2025-04-16 22:25:07 +0100

    changed hostname

--- ui/next.config.mjs
+++ ui/next.config.mjs
@@ -1,20 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns : [{
-            protocol: 'https',
-            hostname: 'lh3.googleusercontent.com',
-            port: '',
-            pathname: '/a/**',
-            
-        },
-        {
-            protocol: 'https',
-            hostname: 's.gravatar.com',
-            port: '',
-            pathname: '/avatar/**',
-            
-        }
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 's.gravatar.com',
+                port: '',
+                pathname: '/avatar/**',
+            }
         ],
     },
 };
